e2eef38cdd5601814967c7d97035da3ae148a791
joeyQuick <joeygquick9113@gmail.com>
2025-05-03 02:34:47 -0500

    Base constants (Works)
    Base preset constants for tuning Pedro. Working circle

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -28,20 +28,20 @@ public class FConstants {
         FollowerConstants.forwardZeroPowerAcceleration = -23.690538546639058;
         FollowerConstants.lateralZeroPowerAcceleration = -63.96228927345531;
 
-        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.2,0,0.035,0);
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.3,0,0.04,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
-        FollowerConstants.headingPIDFCoefficients.setCoefficients(1.75,0,0.2,0.0);
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(1,0,0,0.0);
         FollowerConstants.useSecondaryHeadingPID = false;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.02,0,0.002,0.6,0);
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.01,0,0.0,0,0);
         FollowerConstants.useSecondaryDrivePID = false;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
 
-        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
-        FollowerConstants.centripetalScaling = 0.0018;
+        FollowerConstants.zeroPowerAccelerationMultiplier = 5;
+        FollowerConstants.centripetalScaling = 0.0005;
 
         FollowerConstants.pathEndTimeoutConstraint = 500;
         FollowerConstants.pathEndTValueConstraint = 0.995;
