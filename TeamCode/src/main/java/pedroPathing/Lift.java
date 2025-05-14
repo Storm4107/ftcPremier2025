db1b051df1ad35c84c456970c333ec96693c4e28
joeyQuick <joeygquick9113@gmail.com>
2025-05-14 05:27:48 -0500

    Adjusted elevator object
    Joined the 2 ele motors properly.5/14/2025 - JoeyQ

--- TeamCode/src/main/java/pedroPathing/Lift.java
+++ TeamCode/src/main/java/pedroPathing/Lift.java
@@ -1,64 +1,57 @@
 package pedroPathing;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.rowanmcalpin.nextftc.core.Subsystem;
 import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
 import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.Controllable;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorGroup;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
 
 public class Lift extends Subsystem {
     // BOILERPLATE
     public static final Lift INSTANCE = new Lift();
-    private Lift() { }
+
+    private Lift() {}
 
     // USER CODE
-    public MotorEx leftMotor;
-    public MotorEx rightMotor;
-
     public PIDFController controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0.0));
 
     public String leftName = "leftLift";
     public String rightName = "rightLift";
 
-    public RunToPosition toLow() {
-        new RunToPosition(leftMotor, // MOTOR TO MOVE
-                0.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
+    MotorGroup elevator;
 
-        return new RunToPosition(rightMotor, // MOTOR TO MOVE
+    public RunToPosition toLow() {
+        return new RunToPosition(elevator, // MOTOR TO MOVE
                 0.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
 
     public RunToPosition toMiddle() {
-        new RunToPosition(leftMotor, // MOTOR TO MOVE
-                500.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
-
-        return new RunToPosition(rightMotor, // MOTOR TO MOVE
-                500.0, // TARGET POSITION, IN TICKS
+        return new RunToPosition(elevator, // MOTOR TO MOVE
+                -500.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
 
     public RunToPosition toHigh() {
-        new RunToPosition(leftMotor, // MOTOR TO MOVE
-                1200.0, // TARGET POSITION, IN TICKS
-                controller, // CONTROLLER TO IMPLEMENT
-                this); // IMPLEMENTED SUBSYSTEM
-
-        return new RunToPosition(rightMotor, // MOTOR TO MOVE
-                1200.0, // TARGET POSITION, IN TICKS
+        return new RunToPosition(elevator, // MOTOR TO MOVE
+                -1200.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
 
     @Override
     public void initialize() {
-        leftMotor = new MotorEx(leftName);
-        rightMotor = new MotorEx(rightName);
+
+        MotorEx rightMotor = new MotorEx("rightLift");
+        MotorEx leftMotor = new MotorEx("leftLift");
+
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        elevator = new MotorGroup(rightMotor, leftMotor);
     }
 }
--- TeamCode/src/main/java/pedroPathing/test_auto.java
+++ TeamCode/src/main/java/pedroPathing/test_auto.java
@@ -12,7 +12,6 @@ import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
 import com.rowanmcalpin.nextftc.core.command.utility.delays.Delay;
 import com.rowanmcalpin.nextftc.pedro.FollowPath;
 import com.rowanmcalpin.nextftc.pedro.PedroOpMode;
-
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
@@ -21,41 +20,32 @@ public class test_auto extends PedroOpMode {
     public test_auto() {
         super(Lift.INSTANCE);
     }
-
-    //private final Pose startPose = new Pose(9.0, 60.0, Math.toRadians(0.0));
-    //private final Pose finishPose = new Pose(37.0, 50.0, Math.toRadians(180.0));
-
+    private final Pose startPose = new Pose(8.0, 108.0, Math.toRadians(0.0));
+    private final Pose finishPose = new Pose(24, 108.0, Math.toRadians(0.0));
     private PathChain move;
-
-    /*public void buildPaths() {
+    public void buildPaths() {
         move = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(startPose), new Point(finishPose)))
                 .setLinearHeadingInterpolation(startPose.getHeading(), finishPose.getHeading())
                 .build();
     }
-     */
-
     public SequentialGroup secondRoutine() {
         return new SequentialGroup(
                 new ParallelGroup(
-                        //new FollowPath(move),
+                        new FollowPath(move),
                         Lift.INSTANCE.toHigh()
-                ));
-        /*,
-                new ParallelGroup(
-                        //Claw.INSTANCE.open(),
-                        Lift.INSTANCE.toMiddle()
                 ),
-                new Delay(1.0),
-                Lift.INSTANCE.toLow()
-        );*/
+                //Lift.INSTANCE.toMiddle(),
+                new Delay(1.0)
+                //Lift.INSTANCE.toLow()
+        );
     }
 
     @Override
     public void onInit() {
-        /*Constants.setConstants(FConstants.class, LConstants.class);
-        follower.setStartingPose(new Pose(0, 0, 0));
-        buildPaths();*/
+        follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+        follower.setStartingPose(startPose);
+        buildPaths();
     }
 
     @Override
