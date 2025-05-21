f1e2ae7f0d4b5931a873e99794e6bb6995f8b9df
joeyQuick <joeygquick9113@gmail.com>
2025-05-21 05:49:16 -0500

    elevator testing
    Deos not work. Tested tolerance on the Lift. - 5/21/2025

--- TeamCode/src/main/java/pedroPathing/Lift.java
+++ TeamCode/src/main/java/pedroPathing/Lift.java
@@ -2,56 +2,58 @@ package pedroPathing;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.rowanmcalpin.nextftc.core.Subsystem;
+import com.rowanmcalpin.nextftc.core.command.Command;
 import com.rowanmcalpin.nextftc.core.control.controllers.PIDFController;
 import com.rowanmcalpin.nextftc.core.control.controllers.feedforward.StaticFeedforward;
-import com.rowanmcalpin.nextftc.ftc.hardware.controllables.Controllable;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorGroup;
 import com.rowanmcalpin.nextftc.ftc.hardware.controllables.RunToPosition;
 
+
 public class Lift extends Subsystem {
     // BOILERPLATE
     public static final Lift INSTANCE = new Lift();
-
-    private Lift() {}
+    private Lift() { }
 
     // USER CODE
-    public PIDFController controller = new PIDFController(0.005, 0.0, 0.0, new StaticFeedforward(0.0));
+    public MotorGroup elevator;
 
-    public String leftName = "leftLift";
-    public String rightName = "rightLift";
+    public PIDFController controller = new PIDFController(0.03, 0.0, 0.0, new StaticFeedforward(0.0),100);
 
-    MotorGroup elevator;
+    public String leftLift = "leftLift";
+    public String rightLift = "rightLift";
 
-    public RunToPosition toLow() {
+    public Command toLow() {
         return new RunToPosition(elevator, // MOTOR TO MOVE
                 0.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
 
-    public RunToPosition toMiddle() {
+    public Command toMiddle() {
         return new RunToPosition(elevator, // MOTOR TO MOVE
-                -500.0, // TARGET POSITION, IN TICKS
+                500.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
 
-    public RunToPosition toHigh() {
+    public Command toHigh() {
         return new RunToPosition(elevator, // MOTOR TO MOVE
-                -1200.0, // TARGET POSITION, IN TICKS
+                1000.0, // TARGET POSITION, IN TICKS
                 controller, // CONTROLLER TO IMPLEMENT
                 this); // IMPLEMENTED SUBSYSTEM
     }
+
+
 
     @Override
     public void initialize() {
+        MotorEx leftLift = new MotorEx("leftLift");
+        MotorEx rightLift = new MotorEx("rightLift");
 
-        MotorEx rightMotor = new MotorEx("rightLift");
-        MotorEx leftMotor = new MotorEx("leftLift");
+        rightLift.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftLift.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        elevator = new MotorGroup(rightMotor, leftMotor);
+        elevator = new MotorGroup(leftLift, rightLift);
     }
 }
